5f5746b74bed679b8dcd3a1f93813db80a655df8
Adam Struthers <adam@serouslabs.com>
2017-12-18 21:41:15 -0500

    Cleaned

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -54,18 +54,7 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
           <meta name="description" content="A property listing application" />
         </Helmet>
         <div>
-          <CenteredSection>
-            <H2>
-              <FormattedMessage {...messages.startProjectHeader} />
-            </H2>
-            <p>
-              <FormattedMessage {...messages.startProjectMessage} />
-            </p>
-          </CenteredSection>
           <Section>
-            <H2>
-              <FormattedMessage {...messages.trymeHeader} />
-            </H2>
             <Form onSubmit={this.props.onSubmitForm}>
               <label htmlFor="username">
                 <FormattedMessage {...messages.trymeMessage} />
