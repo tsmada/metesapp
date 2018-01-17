64045c144f26b500e1e2b07a3e53f4463534cb9c
Adam Struthers <adam@serouslabs.com>
2018-01-16 19:05:50 -0500

    Revert "Merged ssr into master"
    This reverts commit 5ecf8e6174b8bd4499ef0554c8756f43b553582f, reversing
    changes made to e061e08ecec120c037dd36f29886ef3d686e62d0.

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,41 +7,62 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
+import { Switch, Route } from 'react-router-dom';
 
+import HomePage from 'containers/HomePage/Loadable';
+import DashboardPage from 'containers/DashboardPage/Loadable';
+import RegistrationPage from 'containers/RegistrationPage/Loadable';
+import LoginPage from 'containers/LoginPage/Loadable';
+import { makeSelectCurrentUser } from 'containers/App/selectors';
+import ItemDetail from 'containers/ItemDetail/Loadable';
+import DashboardContainer from 'containers/DashboardPage';
+import ProfileContainer from 'containers/ProfileContainer';
+import MapContainer from 'containers/MapContainer/Loadable';
+import InvestorContainer from 'containers/InvestorContainer/Loadable';
+import HordeContainer from 'containers/HordeContainer/Loadable';
+import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import { UserAuthWrapper } from 'redux-auth-wrapper';
+import { LOCATION_CHANGE } from 'react-router-redux';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
-import withProgressBar from 'components/ProgressBar';
+import { Authenticated, NotAuthenticated } from './authWrapper';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
+  max-width: 100%;
   margin: 0 auto;
   display: flex;
   min-height: 100%;
   padding: 0 16px;
   flex-direction: column;
+  overflow: none;
 `;
 
-export function App(props) {
+
+export default function App() {
   return (
     <AppWrapper>
       <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
+        titleTemplate="%s - metes.io"
+        defaultTitle="metes.io"
       >
-        <meta name="description" content="A React.js Boilerplate application" />
+        <meta name="description" content="A property listing application" />
       </Helmet>
-      <Header />
-      {React.Children.toArray(props.children)}
+
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+          <Route exact path="/dash" component={Authenticated(DashboardPage)} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/hordes" component={HordeContainer} />
+          <Route exact path="/dash/detail/:id/:address" component={Authenticated(ItemDetail)} />
+          <Route exact path="/user/:id" component={Authenticated(ProfileContainer)}/>
+          <Route exact path="/register" component={RegistrationPage} />
+          <Route exact path="/map" component={MapContainer} />
+          <Route exact path="/invest" component={Authenticated(InvestorContainer)} />
+        <Route path="" component={NotFoundPage} />
+      </Switch>
       <Footer />
     </AppWrapper>
   );
 }
-
-App.propTypes = {
-  children: PropTypes.node,
-};
-
-export default withProgressBar(App);
--- server/middlewares/addProdMiddlewares.js
+++ server/middlewares/addProdMiddlewares.js
@@ -1,8 +1,6 @@
-/* eslint-disable global-require */
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
-const handleSSR = require('./handleSSR');
 
 module.exports = function addProdMiddlewares(app, options) {
   const publicPath = options.publicPath || '/';
@@ -14,5 +12,5 @@ module.exports = function addProdMiddlewares(app, options) {
   app.use(compression());
   app.use(publicPath, express.static(outputPath));
 
-  app.get('*', handleSSR);
+  app.get('*', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')));
 };
