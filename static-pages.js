506547832c883e31e8d3b552f51138a3eebb27b2
Adam Struthers <adam@serouslabs.com>
2018-01-17 15:21:18 -0500

    fix

--- static-pages.js
+++ static-pages.js
@@ -1,13 +1,13 @@
-// Enter your paths here which you want to render as static,
-// Example:,
-// const routes = [,
-//   '/',           // => build/index.html,
-//   '/page',       // => build/page.html,
-//   '/page/',      // => build/page/index.html,
-//   '/page/name',  // => build/page/name.html,
-//   '/page/name/', // => build/page/name/index.html,
-// ];,
-export default [,
+// Enter your paths here which you want to render as static
+// Example:
+// const routes = [
+//   '/',           // => build/index.html
+//   '/page',       // => build/page.html
+//   '/page/',      // => build/page/index.html
+//   '/page/name',  // => build/page/name.html
+//   '/page/name/', // => build/page/name/index.html
+// ];
+export default [
   '/',
   '/dash/',
    '/map/',
